cc769a3905d95266b3e7928efae6b0535b1d80fa
Stefan Mao <karta144593@gmail.com>
2017-05-23 14:38:42 +0800

    {523/1438}

--- app.py
+++ app.py
@@ -373,7 +373,7 @@ def handle_message(event):
 
         return 0
     
-    if blue_ask==True:
+    if  text==call_content:
         buttons_template = TemplateSendMessage(
         alt_text='Buttons template',
         template=ButtonsTemplate(
