fdddf8775752317858af0ca1545e41a764502e7a
Stefan Mao <karta144593@gmail.com>
2017-06-02 11:43:14 +0800

    {2017/6/2 1141}

--- app.py
+++ app.py
@@ -426,7 +426,7 @@ def handle_message(event):
             )
         )
         #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=call_content))
-        line_bot_api.reply_message(event.reply_token,buttons_template)
+        line_bot_api.reply_message(event.reply_token,carousel_template_message)
         return 0
       
        
